ed60cdf13887b79cce71e8f8389dd5e25df85534
Sonal Manwar <74189923+Sonalmanwar@users.noreply.github.com>
2024-04-08 00:55:11 +0530

    Delete src directory

--- src/main/java/com/example/Travel_Booking/Repository/TravelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Travel_Booking.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.Travel_Booking.Model.Travel_Booking;
-
-public interface TravelRepository extends JpaRepository<Travel_Booking, String> {
-
-}
